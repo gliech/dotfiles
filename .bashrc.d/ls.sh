373652f6b9622fb2d3f78b7b3d8c60d0b34966c9
Gregor Bückendorf <gregor.bueckendorf@cbc.de>
2019-01-14 10:16:34 +0100

    bashrc.d: Added some aliases for ls variants(needs further work)

---
+++ .bashrc.d/ls.sh
@@ -0,0 +1,4 @@
+alias ls="ls --color=auto"
+alias ll="ls --human-readable --color=auto -l"
+alias la="ls --human-readable --all --color=auto -l"
+alias lA="ls --human-readable --almost-all --color=auto -l"
