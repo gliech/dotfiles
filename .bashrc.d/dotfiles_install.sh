927e0c5407550592e0e527a90770b20682135e2b
Gregor Bückendorf <gregor.bueckendorf@cbc.de>
2019-05-17 23:07:51 +0200

    aws-cli: Updated the config with tools for saml login

--- .bashrc.d/dotfiles_install.sh
+++ .bashrc.d/dotfiles_install.sh
@@ -32,18 +32,18 @@ if [[ $checksum != $DOTFILES_INSTALL_CHECKSUM ]]; then
       echo "Dotfiles: Downloading vim-plug..."
       if curl \
         --fail \
-	--location \
-	--retry 5 \
-	--retry-delay 1 \
+        --location \
+        --retry 5 \
+        --retry-delay 1 \
         --create-dirs \
-	--progress-bar \
+        --progress-bar \
         --output ~/.local/share/nvim/site/autoload/plug.vim \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
       then
         echo "Dotfiles: Done"
       else
         echo "Dotfiles: Error downloading vim-plug"
-	dotfiles_install_error="yes"
+        dotfiles_install_error="yes"
       fi
     fi
 
