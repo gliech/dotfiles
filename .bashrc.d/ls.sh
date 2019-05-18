34e2071988c14be0536a6a9abda817f53b396c58
Gregor Bückendorf <gregor.bueckendorf@cbc.de>
2019-05-18 15:49:13 +0200

    ls.bashrc: MacOS compatability

--- .bashrc.d/ls.sh
+++ .bashrc.d/ls.sh
@@ -1,4 +1,12 @@
-alias ls="ls --color=auto"
+# On BSD-based operating systems the GNU coreutils version of ls may be
+# installed as gls. I prefer a consistent ls to one that actually integrates
+# well with MacOS, so here we are.
+if command -v gls >/dev/null 2>&1; then
+  alias ls="gls --color=auto"
+else
+  alias ls="ls --color=auto"
+fi
+
 alias ll="ls --human-readable --color=auto -l"
 alias la="ls --human-readable --all --color=auto -l"
 alias lA="ls --human-readable --almost-all --color=auto -l"
