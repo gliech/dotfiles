6497b218972155d544eac065f401598e49a8631c
Gregor Bückendorf <gregor.bueckendorf@cbc.de>
2019-06-24 14:04:31 +0200

    bashrc.d: Overwrite system python with brew python3 via PATH precedence

--- .bashrc.d/brew_caveats.sh
+++ .bashrc.d/brew_caveats.sh
@@ -3,8 +3,11 @@ if [[ $OSTYPE != darwin* ]]; then
   return
 fi
 
-# brew formula bash-completion
+# for brew formula bash-completion@2
 if [[ -r /usr/local/etc/profile.d/bash_completion.sh ]]; then
   export BASH_COMPLETION_COMPAT_DIR=/usr/local/etc/bash_completion.d
   . /usr/local/etc/profile.d/bash_completion.sh
 fi
+
+# for brew formula python
+export PATH=/usr/local/opt/python/libexec/bin:$PATH
