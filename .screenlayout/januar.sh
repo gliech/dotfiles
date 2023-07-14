278cc48674a3695979df0806a337f0288a3a730f
gliech <gliech@users.noreply.github.com>
2023-07-14 19:43:49 +0200

    feat: basic i3/X11 config

---
+++ .screenlayout/januar.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DisplayPort-0 --mode 2560x1440 --pos 0x0 --rotate left --output DisplayPort-1 --off --output DisplayPort-2 --primary --mode 5120x1440 --pos 1440x227 --rotate normal --output HDMI-A-0 --off
